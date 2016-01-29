e220e95c534c06b91540b83197a244b3e5a987c7
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-29 16:45:01 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-    resources :products
-    resources :users, only: [:new, :create]
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+  resources :products
+  resources :sessions, only: [:new, :create, :destroy]
 
     end
 
